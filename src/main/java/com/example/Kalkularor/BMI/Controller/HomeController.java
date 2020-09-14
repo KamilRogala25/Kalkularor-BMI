05951364d181def615337be09d32a65d09680963
Kamil Rogala <rogalak25@gmail.com>
2020-09-14 18:29:36 +0200

    "Added results and link to GitHub"

--- src/main/java/com/example/Kalkularor/BMI/Controller/HomeController.java
+++ src/main/java/com/example/Kalkularor/BMI/Controller/HomeController.java
@@ -13,15 +13,18 @@ public class HomeController {
     public String getHomePage(ModelMap map) {
         map.put("powitanie", "Kalkulator BMI");
         map.put("opis", "Kalkulator BMI (Body Mass Index) daje każdemu możliwość szybkiego i wygodego obliczenia własnego wskaźnika masy ciała. BMI obliczamy dzieląc masę ciała (w kilogramach) przez wzrost do kwadratu (w metrach). Wskaźnik ten wykorzystywany jest przede wszystkim do oceny ryzyka pojawienia się groźnych chorób: miażdżycy, choroby niedokrwiennej serca, udaru mózgu, czy nawet nowotworów. Większość tych chorób jest związana z otyłością i dlatego kalkulator BMI to tak przydatne narzędzie.");
-        map.put("wzrost", "Podaj swój wzrost");
-        map.put("waga", "Podaj swoją wagę");
+        map.put("wzrost", "Podaj swój wzrost w centymetrach");
+        map.put("waga", "Podaj swoją wagę w kilogramach");
         return "home";
     }
 
     @GetMapping("/result")
-    public String getResults(@RequestParam double weight, @RequestParam double height, ModelMap map) {
-        map.put("wynikBmi", Calculator.calculateBmi(weight, height));
-        map.put("opisBmi", Calculator.getBmiDescription(Calculator.calculateBmi(weight, height)));
+    public String getResults(@RequestParam(value = "weight") Integer weight, @RequestParam(value = "height") Integer height, ModelMap map) {
+        Calculator calculator = new Calculator();
+        map.put("wynik", calculator.calculateBmi(weight,height));
+        map.put("opis", calculator.getBmiDescription(calculator.calculateBmi(weight, height)));
+//        map.put("weight", weight);
+//        map.put("height", height);
         return "result";
     }
 
--- src/main/java/com/example/Kalkularor/BMI/Model/Calculator.java
+++ src/main/java/com/example/Kalkularor/BMI/Model/Calculator.java
@@ -1,6 +1,9 @@
 package com.example.Kalkularor.BMI.Model;
 
-public abstract class Calculator {
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Calculator {
 
     public static double roundTwoPlaces(double toRound) {
         toRound = Math.round(toRound * 100.0) / 100.0;
@@ -8,31 +11,29 @@ public abstract class Calculator {
 
     }
 
-    public static double calculateBmi(double weight, double height) {
-        if (height > 3.0) {
-            height /= 100;
-        }
-        double result = weight / Math.pow(height, 2);
+    public double calculateBmi(Integer weight, Integer height) {
+        double height1 = (double) height/100;
+        double result = weight / Math.pow(height1, 2);
         return Calculator.roundTwoPlaces(result);
     }
 
-    public static String getBmiDescription(double bmi) {
+    public String getBmiDescription(double bmi) {
         if (bmi <= 16.0) {
-            return "Wygłodzenie";
+            return "wygłodzenie";
         } else if (bmi >= 16.0 && bmi <= 16.99) {
-            return "Wychudzenie";
+            return "wychudzenie";
         } else if (bmi >= 17.0 && bmi <= 18.49) {
-            return "Niedowaga";
+            return "wiedowagę";
         } else if (bmi >= 18.5 && bmi <= 24.99) {
-            return "Waga prawidłowa";
+            return "wagę prawidłową";
         } else if (bmi >= 25.0 && bmi <= 29.99) {
-            return "Nadwaga";
+            return "nadwagę";
         } else if (bmi >= 30.0 && bmi <= 34.99) {
             return "I stopień otyłości";
         } else if (bmi >= 35.0 && bmi <= 39.99) {
             return "II stopień otyłości";
         } else if (bmi >= 40.0) {
-            return "Skrajna otyłość";
+            return "skrajna otyłość";
         }
         return "";
     }
